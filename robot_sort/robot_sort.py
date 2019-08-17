5a11f30a00e6d3abf781a9163a1b3d990351db82
Timothy Shores <timothymshores@gmail.com>
2019-08-16 19:59:04 -0500

    Completed MVP

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -107,9 +107,8 @@ class SortingRobot:
         if self.is_sorted():
             return self.list()
         else:
-            len = self.list_length()
-            for i in range(len):
-                for j in range(0, len - i - 1):
+            for i in range(self.list_length()):
+                for j in range(0, self.list_length() - i - 1):
                     if self.list()[j] > self.list()[j + 1]:
                         self.swap_at(j)
 
@@ -152,7 +151,7 @@ if __name__ == "__main__":
     # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
     #      45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
-    l = [5, 2, 3, 1, 4]
+    l = [1, 2, 6, 4, 5, 3]
     robot = SortingRobot(l)
 
     robot.sort()
